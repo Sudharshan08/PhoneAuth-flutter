d17ae1e6942e5cd404e665141f215097dcd13174
dharshan <sudhuind08@gmail.com>
2024-10-07 18:56:39 +0530

    initial design with firebase integration commit1

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/screens/welcome_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+
+
+
+Future<void>main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(options:DefaultFirebaseOptions.currentPlatform);
+  runApp(const MyApp());
+}
+//void main(){
+  //runApp(const MyApp());
+//}
+
+
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: WelcomeScreen(),
+      title: "FlutterPhoneAuth",
+    );
+  }
+}
